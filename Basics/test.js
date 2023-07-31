625e494cff562ea023fd9284bc4828a8f52a7546
asadatiq1 <asadatiq5@gmail.com>
2023-07-31 18:13:17 +0500

    JV-1

---
+++ Basics/test.js
@@ -0,0 +1,18 @@
+var personActions = {
+  getFullName() {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+function createPerson(firstName, lastName) {
+  let person = Object.create(personActions);
+  person.firstName = firstName;
+  person.lastName = lastName;
+  return person;
+}
+
+let person1 = createPerson("John", "Doe");
+let person2 = createPerson("Jane", "Doe");
+
+console.log(person1.getFullName());
+console.log(person2.getFullName());
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Javascript-test
+
 A code repo for javascript
+
+<!--
+---------- Most Important Topics ----------
+-> JavaScript array methods.
+-> Strings
+-> Objects
+-> Factory functions
+-> Constructor functions
+-> Interpretor and compiler
+-> V8 engine
+-> Hoisting
+-->
